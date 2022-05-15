1f3ee53f43da96a79f0d15b3a0a6221a69b910bf
student <i.i.f@mail.ru>
2022-05-15 23:54:47 +0800

    Initial commit

---
+++ src/main/java/pro/sky/maplesson/MapLessonApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.maplesson;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MapLessonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MapLessonApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pro/sky/maplesson/MapLessonApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.maplesson;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MapLessonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
